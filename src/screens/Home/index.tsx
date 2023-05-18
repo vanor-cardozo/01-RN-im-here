1d00bc198078454082f69642f77eb1d724c2547c
vanor-cardozo <vanor.fc@gmail.com>
2023-05-18 16:25:37 -0300

    feat: Adiciona botão e input a tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,33 @@
-import { Text, View } from "react-native";
-import { styles } from './styles'
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+import { Participant } from '../../components/Participant';
 
-export default function Home(){
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('clicou em adicionar');
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-          Nome do evento
-        </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-        <Text style={styles.eventDate}>
-          Sexta, 4 de novembro de 2023.
-        </Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2023.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
-  )
+  );
 }
