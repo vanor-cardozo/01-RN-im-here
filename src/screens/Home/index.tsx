d77bca3cb36bc4a238a7862fa162d501e0fbba01
vanor-cardozo <vanor.fc@gmail.com>
2023-05-19 12:09:47 -0300

    feat: Utilizando Hooks para controlar o estado da aplicação

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,33 @@
-import { Text, TextInput, View, TouchableOpacity, FlatList } from 'react-native';
+import { useState } from 'react';
+import { Text, TextInput, View, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
 export function Home() {
-  const participants = [
-    'Vanor',
-    'Amanda',
-    'Luiza',
-    'Jean',
-    'Théo',
-    'Isa',
-    'Pedro',
-    'Arthur',
-    'Vanessa',
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    console.log('clicou em adicionar');
+    if (participants.includes(participantName)) {
+      return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome');
+    }
+
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string) {
-    console.log(`clicou em remover ${name}`);
+    Alert.alert('Remover', `Deseja remover o participante ${name}`, [
+      {
+        text: 'Sim',
+        onPress: () =>
+          setParticipants((prevState) => prevState.filter((participant) => participant !== name)),
+      },
+      {
+        text: 'Não',
+        style: 'cancel',
+      },
+    ]);
   }
 
   return (
@@ -34,6 +41,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
@@ -52,17 +61,6 @@ export function Home() {
           <Text style={styles.listEmptyText}>Nenhum participante foi adicionado</Text>
         )}
       />
-
-      {/* Habilitando rolagem da tela com ScrollView
-      <ScrollView>
-        {participants.map((participant) => (
-          <Participant
-            key={participant}
-            name={participant}
-            onRemove={() => handleParticipantRemove(participant)}
-          />
-        ))}
-      </ScrollView> */}
     </View>
   );
 }
